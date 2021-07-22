1d6f2c50827fc02f2da3395d01cea1577a4c71ee
KyQiao <kqiao@connect.ust.hk>
2021-07-22 11:54:58 +0800

    read more info

--- src/logReader.hpp
+++ src/logReader.hpp
@@ -7,6 +7,19 @@
 #include <unordered_map>
 #include <vector>
 
+enum readFlag
+// in the order of output
+{
+  NONE,
+  BOX,
+  ATOMNUMBER,
+  SPACING,
+  THERMO,
+  PERFORMANCE,
+  // not implement yet
+  NEIGHBORINFO,
+};
+
 using d_data = std::vector<double>;
 using thermo = struct thermoData {
   thermoData(){};
@@ -14,6 +27,26 @@ using thermo = struct thermoData {
   std::vector<d_data> attr_table;                      //where the rest properties store [property][particle ID]
   std::string attr_order;                              //i.e. "q c_pe c_voro[1] c_ackland ... "
   std::unordered_map<std::string, size_t> attr_index;  //dict store index and names. i.e. d["q"]=0 d["c_pe"]=1
+
+  double particleN;
+  // lx | ly | lz | hx | hy | hz
+  double box[6];
+  // vx | vy | vz
+  double lattice[3];
+  //tau/day | timesteps/s
+  double performance[2];
+  //  min time  |  avg time  |  max time  | %varavg | %total
+  double Pair[5], Neigh[5], Comm[5], Output[5], Modify[5];
+  //  avg time |  %total
+  double Other[2];
+  // CPU usage | MPI tasks | OpenMP thread
+  double cpu[3];
+
+  bool Box_flag = false,
+       Particle_flag = false,
+       lattice_flag = false,
+       performance_flag = false,
+       Thermo_flag = false;
 
   d_data &get(std::string attr) {
     size_t index;
@@ -32,39 +65,66 @@ using thermo = struct thermoData {
   }
 
   void describe() {
-    std::stringstream ss(this->attr_order);
-    std::string token;
-    std::cout << "thermo data attributes are:\n";
+    if (this->Box_flag) {
+      std::cout << "Box size:\n"
+                << "xl:" << this->box[0] << " xh:" << this->box[3] << "\n"
+                << "yl:" << this->box[1] << " yh:" << this->box[4] << "\n"
+                << "zl:" << this->box[2] << " zh:" << this->box[5]
+                << std::endl;
+    }
 
-    while (ss >> token)
-      std::cout << std::left << std::setw(12) << token;
-    std::cout << "\n";
-    if (this->size() > 10) {
-      for (size_t i = 0; i < 5; i++) {
-        for (size_t j = 0; j < this->attr_table.size(); j++) {
-          std::cout << std::left << std::setw(12) << this->attr_table[j][i];
-        }
-        std::cout << "\n";
-      }
-      for (size_t i = 0; i < this->attr_table.size(); i++)
-        std::cout << std::left << std::setw(12) << "...";
+    if (this->Particle_flag) {
+      std::cout << "Using " << this->particleN << " particles" << std::endl;
+    }
+
+    if (this->lattice_flag) {
+      std::cout << "Lattice constant: "
+                << this->lattice[0] << " "
+                << this->lattice[1] << " "
+                << this->lattice[2] << std::endl;
+    }
+
+    if (this->Thermo_flag) {
+      std::stringstream ss(this->attr_order);
+      std::string token;
+      std::cout << "thermo data attributes are:\n";
+
+      while (ss >> token)
+        std::cout << std::left << std::setw(12) << token;
       std::cout << "\n";
+      if (this->size() > 10) {
+        for (size_t i = 0; i < 5; i++) {
+          for (size_t j = 0; j < this->attr_table.size(); j++) {
+            std::cout << std::left << std::setw(12) << this->attr_table[j][i];
+          }
+          std::cout << "\n";
+        }
+        for (size_t i = 0; i < this->attr_table.size(); i++)
+          std::cout << std::left << std::setw(12) << "...";
+        std::cout << "\n";
 
-      for (size_t i = this->size() - 5; i < this->size(); i++) {
-        for (size_t j = 0; j < this->attr_table.size(); j++) {
-          std::cout << std::left << std::setw(12) << this->attr_table[j][i];
+        for (size_t i = this->size() - 5; i < this->size(); i++) {
+          for (size_t j = 0; j < this->attr_table.size(); j++) {
+            std::cout << std::left << std::setw(12) << this->attr_table[j][i];
+          }
+          std::cout << "\n";
         }
-        std::cout << "\n";
-      }
-    } else {
-      for (size_t i = 0; i < this->size(); i++) {
-        for (size_t j = 0; j < this->attr_table.size(); j++) {
-          std::cout << std::left << std::setw(12) << this->attr_table[j][i];
+      } else {
+        for (size_t i = 0; i < this->size(); i++) {
+          for (size_t j = 0; j < this->attr_table.size(); j++) {
+            std::cout << std::left << std::setw(12) << this->attr_table[j][i];
+          }
+          std::cout << "\n";
         }
-        std::cout << "\n";
       }
     }
-  }
+
+    if (this->performance_flag) {
+      std::cout << "Performance:" << this->performance[0] << " tau/day, " << this->performance[1] << " timesteps/s\n"
+                << this->cpu[0] << "% CPU use with " << this->cpu[1] << " MPI tasks x " << this->cpu[2] << " OpenMP threads"
+                << std::endl;
+    }
+  };
 };
 
 class logReader {
@@ -77,13 +137,13 @@ public:
     read(file);
   };
   ~logReader() {
-    for (size_t i = 0; i < getSize(); i++) {
-      delete runs[i];
+    for (auto v : runs) {
+      delete v;
     }
   };
 
   void read(std::string);
-  int checker(std::string);
+  readFlag checker(std::string);
 
   // return # of thermoData
   size_t getSize() const {
